951e550d74028cfa26523acbda7cd7e8286f7f14
atsed27 <danielnigatu09@gmail.com>
2023-12-08 10:38:55 -0300

    navbar is done

--- app/index.js
+++ app/index.js
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native';
+import { COLORS, SIZES, icons, images } from '../constants';
+import { Stack } from 'expo-router';
+import ScreenHeaderBtn from '../components/common/header/ScreenHeaderBtn';
+import Welcome from '../components/home/welcome/Welcome';
+import Popularjobs from '../components/home/popular/Popularjobs';
+import Nearbyjobs from '../components/home/nearby/Nearbyjobs';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home page</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: true,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: '',
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
